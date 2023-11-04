1efdf7d468480f32671311fc0a288a0ba9932844
zhaotan18x <5090152-zhaotan18x@users.noreply.replit.com>
2023-11-04 08:35:25 +0000

    readme update and new echo
    nothing special

--- main.sh
+++ main.sh
@@ -3,3 +3,6 @@ echo Hello World
 echo "write your name:"
 read name
 echo "Your Name is: $name"
+
+now=$(date +"%T")
+echo "Current time : $now"
---
+++ readme.md
@@ -0,0 +1,7 @@
+# bash example push to github
+
+## sub text 
+for text from repl.it
+
+
+https://replit.com/@zhaotan18x/AnxiousDevotedModule
